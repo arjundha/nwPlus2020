d3e113c3a773c20002d8abe04c5e226c300029a0
Robert Yip <robertyip88@gmail.com>
2020-12-06 18:44:57 -0500

    data object and summary

--- schedule.js
+++ schedule.js
@@ -1,18 +1,18 @@
 //list of timeslots
 let data = [
 
-        /*
-        id: "",
-        name: "",
-        day: "",
-        time: 0,
-        duration: 0, //in minutes    
-        desc: "",  //some text
-        type: "",
-        color: "",
-        active: true
-        */
-    
+    /*
+    id: "",
+    name: "",
+    day: "",
+    time: 0,
+    duration: 0, //in minutes    
+    desc: "",  //some text
+    type: "",
+    color: "",
+    active: true
+    */
+
 ]
 
 const sunlightStart = 5;
@@ -24,25 +24,27 @@ function initObjects() {
 
     for (let i = 0; i < slots.length; i++) {
         let slotstr = slots[i].id;
-        data.push(makeSlot(i, "", slotstr.slice(0,3), slotstr.slice(3), 0, "", "", "", false));
-        //slots[i].addEventListener("click", initbutton(i));
+        data.push(makeSlot(i, "", slotstr.slice(0, 3), slotstr.slice(3), 0, "", "", "", false));
+        slots[i].addEventListener("click", function () {
+            let bName = data[i].name;
+            let bDay = data[i].day;
+            let bTime = data[i].time;
+            let bDuration = data[i].duration;
+            let bDesc = data[i].desc;
+            let bType = data[i].type;
+            updateSlot(i, bName, bDay, bTime, bDuration, bDesc, bType);
+
+
+        });
     }
+
+
+
     render();
 
 
 }
 
-function initButton(i) {
-    //form visible
-    //get form items, and prefill with data[i]
-    let buttonName = data[i].name;
-    let buttonDay = data[i].day;
-    let buttonTime = data[i].time;
-    let buttonDuration = data[i].duration;
-    let buttonDesc = data[i].desc;
-    let buttonType = data[i].type;
-
-}
 
 function makeSlot(i, sname, sday, stime, sdur, sdesc, stype, scolor, sactive) {
     return {
@@ -62,14 +64,38 @@ function makeSlot(i, sname, sday, stime, sdur, sdesc, stype, scolor, sactive) {
 
 }
 
-function updateSlot(s) {
-    let getName = document.getElementById("");
-    let getDay = document.getElementById("");
-    let getTime = document.getElementById("");
-    let getDuration = document.getElementById("");
-    let getDesc = document.getElementById("");
-    let getType = document.getElementById("");
+function updateSlot(i, uName, uDay, uTime, uDuration, uDesc, uType) {
+    let fName = document.getElementById("");
+    let fDay = document.getElementById("");
+    let fTime = document.getElementById("");
+    let fDuration = document.getElementById("");
+    let fDesc = document.getElementById("");
+    let fType = document.getElementById("");
     //let id = //Get id
+    fName = uName;
+    fDay = uDay;
+    fTime = uTime;
+    fDuration = uDuration;
+    fDesc = uDesc;
+    fType = uType;
+
+    submitButton = document.getElementById("")
+    submitButton.addEventListener("click", function () {
+        data[i].name = uName;
+        data[i].day = uDay;
+        data[i].time = uTime;
+        data[i].duration = uDuration;
+        data[i].desc = uDesc;
+        data[i].type = uType;
+        render();
+    })
+
+    getName = "gregor 101";
+    getDay = "mon";
+    getTime = "0800";
+    getDuration = "1";
+    getDesc = "trust the natural recursion";
+    getType = "Class";
 
     nameValue = "";
     dayValue = "";
@@ -84,14 +110,9 @@ function updateSlot(s) {
             dayValue = i + 1;
         }
     }
-    for (let i = 0; i < getHour.length; i++) {
-        if (getHour[i].checked) {
-            hourValue = getHour[i].text;
-        }
-    }
-    for (let i = 0; i < getMin.length; i++) {
-        if (getMin[i].checked) {
-            minValue = getMin[i].text;
+    for (let i = 0; i < getTime.length; i++) {
+        if (getTime[i].checked) {
+            timeValue = getTime[i].text;
         }
     }
     for (let i = 0; i < getDuration.length; i++) {
@@ -111,9 +132,9 @@ function updateSlot(s) {
     else if (getType == 3) { colorValue = "FFFFFF"; }
     else if (getType == 4) { colorValue = "FFFFFF"; }
 
-    data[id] = makeSlot(id, nameValue, dayValue, hourValue, minValue, durValue, typeValue, colorValue, true);
+    data[id] = makeSlot(id, nameValue, dayValue, timeValue, durValue, typeValue, colorValue, true);
     render();
-    //}
+
 }
 
 
@@ -129,13 +150,93 @@ function switchTimezone(t) {
 
 function createSummary() {
     let day = [];
-    for (let i = 0; i < 5; i++)
+    for (let i = 0; i < 5; i++) {
         day.push({
-            classTime: 0,
+            classTime: 0,        //minutes
             exerciseTime: 0,
             leisureTime: 0,
-            studyTime: 0,
+            studyTime: 0
         })
+    }
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < data.length; j++) {
+            if (i == 0 && data[j].day == "mon") {
+                if (data[j].type == "Class") {
+                    day[i].classTime = day[i].classTime + data[j].duration
+                }
+                else if (data[j].type == "Exercise"){
+                    day[i].exerciseTime = day[i].exerciseTime + data[j].duration
+                }
+                else if (data[j].type == "Leisure"){
+                    day[i].leisureTime = day[i].leisureTime + data[j].duration
+                }
+                else if (data[j].type == "Study"){
+                    day[i].studyTime = day[i].studyTime + data[j].duration
+                }
+            }
+            if (i == 1 && data[j].day == "tue") {
+                if (data[j].type == "Class") {
+                    day[i].classTime = day[i].classTime + data[j].duration
+                }
+                else if (data[j].type == "Exercise"){
+                    day[i].exerciseTime = day[i].exerciseTime + data[j].duration
+                }
+                else if (data[j].type == "Leisure"){
+                    day[i].leisureTime = day[i].leisureTime + data[j].duration
+                }
+                else if (data[j].type == "Study"){
+                    day[i].studyTime = day[i].studyTime + data[j].duration
+                }
+            }
+            if (i == 2 && data[j].day == "wed") {
+                if (data[j].type == "Class") {
+                    day[i].classTime = day[i].classTime + data[j].duration
+                }
+                else if (data[j].type == "Exercise"){
+                    day[i].exerciseTime = day[i].exerciseTime + data[j].duration
+                }
+                else if (data[j].type == "Leisure"){
+                    day[i].leisureTime = day[i].leisureTime + data[j].duration
+                }
+                else if (data[j].type == "Study"){
+                    day[i].studyTime = day[i].studyTime + data[j].duration
+                }
+            }
+            if (i == 3 && data[j].day == "thu") {
+                if (data[j].type == "Class") {
+                    day[i].classTime = day[i].classTime + data[j].duration
+                }
+                else if (data[j].type == "Exercise"){
+                    day[i].exerciseTime = day[i].exerciseTime + data[j].duration
+                }
+                else if (data[j].type == "Leisure"){
+                    day[i].leisureTime = day[i].leisureTime + data[j].duration
+                }
+                else if (data[j].type == "Study"){
+                    day[i].studyTime = day[i].studyTime + data[j].duration
+                }
+            }
+            if (i == 4 && data[j].day == "fri") {
+                if (data[j].type == "Class") {
+                    day[i].classTime = day[i].classTime + data[j].duration
+                }
+                else if (data[j].type == "Exercise"){
+                    day[i].exerciseTime = day[i].exerciseTime + data[j].duration
+                }
+                else if (data[j].type == "Leisure"){
+                    day[i].leisureTime = day[i].leisureTime + data[j].duration
+                }
+                else if (data[j].type == "Study"){
+                    day[i].studyTime = day[i].studyTime + data[j].duration
+                }
+            }
+
+
+        }
+
+    }
+
+
     let getClassTime = document.getElementsByClassName("");
     let getExerciseTime = document.getElementsByClassName("");
     let getLeisureime = document.getElementsByClassName("");
@@ -147,7 +248,7 @@ function createSummary() {
         getLeisureime[i].innerHTML = day[i].leisureTime;
         getStudyTime[i].innerHTML = day[i].studyTime;
     }
-    //render();
+    render();
 }
 
 
@@ -157,8 +258,9 @@ function render() {
     data[10].time = "0800";
     data[10].active = true;
     for (let i = 0; i < data.length; i++) {
-        if (data[i].active == true){
-            slots[i].innerText =  data[i].name + " " + data[i].time;
+        if (data[i].active == true) {
+            slots[i].innerHTML = '<div class="event triple"><input id="check" type="checkbox" class="checkbox" /><label for="check"></label>' +
+                data[10].name + ' ' + data[10].time + '</div>'
 
         }
     }
